e2521a04bf98d65bae9b2aad07c125f8da865676
zer0 <max.hasselwander@wiss-edu.ch>
2025-02-24 11:25:15 +0100

    Updated engine.h and make a new Name convetions

--- src/headers/engine.h
+++ src/headers/engine.h
@@ -7,27 +7,27 @@
 typedef struct {
     float x;
     float y;
-} Vector2;
+} ZFB_Vector2;
 
 // This stores the physics properties of an entity
 typedef struct {
-    Vector2 position; // Where the object is
-    Vector2 velocity; // How fast it's moving
-    Vector2 acceleration; // How much force is applied
+    ZFB_Vector2 position; // Where the object is
+    ZFB_Vector2 velocity; // How fast it's moving
+    ZFB_Vector2 acceleration; // How much force is applied
     float mass; // How heavy it is
-} PhysicsBody; 
+} ZFB_PhysicsBody; 
 
 // This represents a game entity (Player or Wall)
 typedef struct {
     int id; // Unique identifier
-    PhysicsBody physics; // Physics properties
+    ZFB_PhysicsBody physics; // Physics properties
     int width, height; // Size (for collision detection)
-} Entity;
+} ZFB_Entity;
 
 // Function declarations 
-void apply_force(Entity *entity, Vector2 force);
-void update_physics(Entity *entity, float dt);
-int check_collision(Entity *a, Entity *b);
+void apply_force(ZFB_Entity *entity, ZFB_Vector2 force);
+void update_physics(ZFB_Entity *entity, float dt);
+int check_collision(ZFB_Entity *a, ZFB_Entity *b);
 
 #endif
 
