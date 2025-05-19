8b72d69f3178905c22b2b47804b0f40123d93a01
bomba-cat <bledion.morina@hotmail.com>
2025-05-19 15:33:49 +0200

    Added lib path to comb file

--- comb.sh
+++ comb.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Used to combined all the lib files
-# into a big fat one
+# into one big library
+
+LIB_DIR=build/lib
 
 ar -M <<EOM
-CREATE libZFB_fat.a
-ADDLIB libzfb.a
-ADDLIB libpng.a
-ADDLIB libz.a
+CREATE ${LIB_DIR}/libZFB_fat.a
+ADDLIB ${LIB_DIR}/libzfb.a
+ADDLIB ${LIB_DIR}/libpng.a
+ADDLIB ${LIB_DIR}/libz.a
 SAVE
 END
 EOM
