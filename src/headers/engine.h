f8e243525fb84afc8add10e372d7d156e00c835b
zer0 <max.hasselwander@wiss-edu.ch>
2025-05-26 14:21:53 +0200

    included math.h and stopped using own definitions

--- src/headers/engine.h
+++ src/headers/engine.h
@@ -4,23 +4,8 @@
 #define ZFB_ROT_EPS  0.001f
 #include <stdint.h>
 #include <stdbool.h>
+#include <math.h>
 
-#define M_PI   3.14159265358979323846f
-#define M_TAU  6.28318530717958647692f
-
-static inline float zfb_sinf(float x){
-        while (x >  M_PI) x -= M_TAU;
-        while (x < -M_PI) x += M_TAU;
-        float x2 = x * x;
-        return x * (1.0f - x2 / 6.0f + x2 * x2 / 120.0f);
-}
-static inline float zfb_cosf(float x) { return zfb_sinf(x + M_PI * 0.5f); }
-#define sinf(x)  zfb_sinf(x)
-#define cosf(x)  zfb_cosf(x)
-
-static inline float zfb_fabsf(float x) { return x < 0.0f ? -x : x; }
-#define fabsf(x) zfb_fabsf(x)
- 
 typedef struct {
     float x;
     float y;
