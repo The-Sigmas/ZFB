50ee18baa85dc1980b74538be7950171152c1a43
bomba-cat <bledion.morina@hotmail.com>
2025-06-06 10:36:41 +0200

    fixed #29 by adding a FrameLimiter

---
+++ src/engine/framelimiter.c
@@ -0,0 +1,27 @@
+#include "../headers/framelimiter.h"
+
+uint32_t frameLimitCount = 0;
+ZFB_FrameLimiter* frameLimiters = NULL;
+
+uint32_t ZFB_PushFrameLimiter(ZFB_FrameLimiter newFrameLimit)
+{
+  frameLimitCount++;
+  frameLimiters = realloc(frameLimiters, sizeof(ZFB_FrameLimiter) * frameLimitCount);
+  frameLimiters[frameLimitCount=1] = newFrameLimit;
+  return frameLimitCount;
+}
+
+void frameTick()
+{
+  for(int frameLimit = 0; frameLimit < frameLimitCount; frameLimit++)
+  {
+    if(frameLimiters[frameLimit].frame < frameLimiters[frameLimit].limit)
+    {
+      frameLimiters[frameLimit].frame++;
+    } else
+    {
+      frameLimiters[frameLimit].func(frameLimiters[frameLimit].params);
+      frameLimiters[frameLimit].frame = 0;
+    }
+  }
+}
